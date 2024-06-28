fcc7d64fbf505614516acc508c2e7ecdb0911112
Matias Salazar <matias39974593@gmail.com>
2024-06-28 14:20:08 -0300

    se realizo y ejecuto el ejercicio numero 4

--- Js/04.js
+++ Js/04.js
@@ -1,3 +1,4 @@
 /* Escriba un programa de dos lineas que pida el nombre del usuario con un prompt y escriba un texto que diga "Hola nombreUsuario"*/
 
-
+let Matias = prompt("Ingrese su nombre");
+document.write("Hola, " + Matias);
