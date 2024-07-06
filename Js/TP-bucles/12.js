cf3f7712eec0c2ff803183c17626e40fb71db984
Matias Salazar <matias39974593@gmail.com>
2024-07-06 16:15:04 -0300

    ejercicio numero 12 resuelto

---
+++ Js/TP-bucles/12.js
@@ -0,0 +1,6 @@
+//Realiza un script que genere un número aleatorio entre 1 y 99
+
+let numeroAleatorio =
+Math.floor(Math.random() * 99) + 1;
+
+document.write(`Numero aleatorio entre 1 y 99: ${numeroAleatorio}`);
