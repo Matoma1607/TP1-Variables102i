e70e66c827bdcd4135a984565de4f89e5eb0bbec
Matias Salazar <matias39974593@gmail.com>
2024-07-06 16:25:42 -0300

    ejercicio numero 13 ejecutado

---
+++ Js/TP-bucles/13.js
@@ -0,0 +1,6 @@
+//  Realiza un script que pida un texto y lo muestre en mayúsculas.
+
+let texto = prompt("Ingrese un texto:");
+
+let textoMayusculas = texto.toUpperCase();
+document.write(`Texto en mayusculas: ${textoMayusculas}`);
