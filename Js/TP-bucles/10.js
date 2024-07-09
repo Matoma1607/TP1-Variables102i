e65f3746de586cf1819a3465cc39e86f800a758b
Matias Salazar <matias39974593@gmail.com>
2024-07-09 18:08:25 -0300

    dibujamos el numero de filas que pide el usuario

--- Js/TP-bucles/10.js
+++ Js/TP-bucles/10.js
@@ -4,12 +4,13 @@ const filas = parseInt(prompt("Ingrese un numero de filas"));
 const columnas = parseInt(prompt("Ingrese un numero de filas"));
 
 document.write("<table> <tbody>");
-document.write("<tr>");
 
-for(let indiceColumnas = 0; indiceColumnas - columnas; indiceColumnas++){
-    document.write("<td>1</td>");
+for(let indiceFilas = 0; indiceFilas - filas; indiceFilas++){
+    document.write("<tr>");
+    for(let indiceColumnas = 0; indiceColumnas - columnas; indiceColumnas++){
+        document.write("<td>1</td>");
+    }
+    document.write("</tr>");
 }
 
-
-document.write("</tr>");
 document.write("</tbody></table>");
