4604638d8a385d0152d3be4214319ac629fd3e30
Matias Salazar <matias39974593@gmail.com>
2024-07-09 18:00:42 -0300

    dibujamos numeros de columnas

---
+++ Js/TP-bucles/10.js
@@ -0,0 +1,15 @@
+// Realiza un script que pida número de filas y columnas y escriba una tabla. Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
+
+const filas = parseInt(prompt("Ingrese un numero de filas"));
+const columnas = parseInt(prompt("Ingrese un numero de filas"));
+
+document.write("<table> <tbody>");
+document.write("<tr>");
+
+for(let indiceColumnas = 0; indiceColumnas - columnas; indiceColumnas++){
+    document.write("<td>1</td>");
+}
+
+
+document.write("</tr>");
+document.write("</tbody></table>");
