a080b9bb7f670ce80b8109631779ba0889c023ac
Matias Salazar <matias39974593@gmail.com>
2024-07-09 18:17:05 -0300

    agregamos valor de las celdas en la tabla

--- Js/TP-bucles/10.js
+++ Js/TP-bucles/10.js
@@ -2,15 +2,16 @@
 
 const filas = parseInt(prompt("Ingrese un numero de filas"));
 const columnas = parseInt(prompt("Ingrese un numero de filas"));
-
+let totalCeldas = filas * columnas;
 document.write("<table> <tbody>");
 
-for(let indiceFilas = 0; indiceFilas - filas; indiceFilas++){
-    document.write("<tr>");
-    for(let indiceColumnas = 0; indiceColumnas - columnas; indiceColumnas++){
-        document.write("<td>1</td>");
-    }
-    document.write("</tr>");
+for (let indiceFilas = 0; indiceFilas - filas; indiceFilas++) {
+  document.write("<tr>");
+  for (let indiceColumnas = 0; indiceColumnas - columnas; indiceColumnas++) {
+    document.write(`<td>${totalCeldas}</td>`);
+    totalCeldas--;
+  }
+  document.write("</tr>");
 }
 
 document.write("</tbody></table>");
