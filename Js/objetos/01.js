ff3030f080894ea1a56eb2d20ffe113a0cf66b45
Matias Salazar <matias39974593@gmail.com>
2024-07-21 16:06:07 -0300

    se terminó de ejecutar el ejercicio 01 de objetos

--- Js/objetos/01.js
+++ Js/objetos/01.js
@@ -4,7 +4,27 @@ const auto = {
     color: 'Negro',
     marca: 'Toyota',
     modelo: 'Corolla',
-    encendido: false,
+    estado: 'apagado',
     año: 2024,
+    motor: ['Motor 2.4 D-4D', 'Propulsor diésel de cuatro cilindros', '150 CV de potencia máxima'],
     
-}
+    encender: function(){
+        if (this.estado === 'apagado'){
+            this.estado = "encendido";
+            document.write("<p> El auto  " + this.marca + " " + this.modelo + " ha sido encendido. </p>");
+        }else {
+            document.write("<p> El auto ya está encendido </p>");
+        }
+    },
+    apagar: function(){
+        if (this.estado === 'encendido'){
+            this.estado = "apagado";
+            document.write("<p> El auto  " + this.marca + " " + this.modelo + " ha sido apagado. </p>");
+        }else{
+            document.write("<p> El auto ya está apagado </p>");
+        }
+    }
+};
+auto.encender();
+
+auto.apagar();
