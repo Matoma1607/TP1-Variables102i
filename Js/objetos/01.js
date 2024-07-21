88c988cfcb5d2dce856834357ab02c1cd8683c49
Matias Salazar <matias39974593@gmail.com>
2024-07-20 21:55:38 -0300

    falta completar el ejercicio 01 de objeto

---
+++ Js/objetos/01.js
@@ -0,0 +1,10 @@
+/* Crea un objeto llamado auto que tenga algunas características como el color, marca, modelo y si está encendido o apagado. Crea los métodos necesarios para permitir encender y apagar el auto.*/
+
+const auto = {
+    color: 'Negro',
+    marca: 'Toyota',
+    modelo: 'Corolla',
+    encendido: false,
+    año: 2024,
+    
+}
