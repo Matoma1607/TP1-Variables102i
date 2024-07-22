523e69390c1ac246da3eb29f3c8823006ef3cc1b
Matias Salazar <matias39974593@gmail.com>
2024-07-22 17:27:28 -0300

    se terminó de corregir el ejercicio numero 02

--- Js/objetos/02.js
+++ Js/objetos/02.js
@@ -1,34 +1,36 @@
 //Crea un objeto persona que contenga las propiedades nombre, edad y hobbies (un array de strings). Luego, escribe una función que reciba un objeto persona y un nuevo hobby y añada este hobby a la lista de hobbies de la persona. Finalmente, imprime los hobbies de la persona usando un bucle. Usa prompt para ingresar los datos de la persona y el nuevo hobby.
 
-function crearPersona(nombre, edad, hobbies) {
-    return{
-        nombre: "Antonella",
-        edad: 25,
-        hobbies: "leer" || [],
-    };
+class Persona {
+  constructor(nombre, edad, hobbies) {
+    this.nombre = "Antonella";
+    this.edad = 25;
+    this.hobbies = [" leer ", " entrenar ", " fotografia "];
+  }
 }
 
-function nuevoHobby(persona, nuevoHobby) {
-  persona.hobbies.push(nuevoHobby);
+function agregarHobby(persona, nuevoHobby) {
+  persona.hobbies.push(" ciclismo ");
 }
 
 function imprimirHobbies(persona) {
-    document.write(`${persona.nombre} tiene los siguientes hobbies:`);
-    for (const hobby of persona.hobbies) {
-        document.write(hobby);
-      }
-  } 
+  document.write(`${persona.nombre} tiene los siguientes hobbies:`);
+  for (const hobby of persona.hobbies) {
+    document.write(hobby);
+  }
+}
 
 const nombre = prompt(" Ingrese su nombre: ");
 const edad = parseInt(prompt(" Ingrese su edad: "));
-const hobbies = prompt(" Ingrese sus hobbies separados por comas: ")
-  .split(",")
-  .map((hobby) => hobby.trim());
+const hobbiesIniciales = prompt(" Ingrese sus hobbies iniciales separados por comas: ").split(",");
 
-  const persona = crearPersona(nombre, edad, hobbies);
-  //const nuevoHobby = prompt("Ingrese un nuevo hobby para añadir a la persona:");
+const persona = new Persona(nombre, parseInt(edad), hobbiesIniciales);
+const nuevoHobby = prompt("Ingrese un nuevo hobby para añadir a la persona:");
 
-  nuevoHobby(persona, nuevoHobby);
-  imprimirHobbies(persona);
+if (nuevoHobby.trim() === "") {
+  agregarHobby(persona);
+} else {
+  agregarHobby(persona, nuevoHobby);
+}
 
+imprimirHobbies(persona);
 
